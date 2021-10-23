c3a5c00bac14cf28db51ad325adae3e59c90c33a
thanhren <40245250+thanhren@users.noreply.github.com>
2021-10-23 15:11:38 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://docs.google.com/forms/d/12d8p9ckl-BJ1i0IuKnLXBQmhNZaOb-W20zEVngXO2uk/";
+            window.location = "https://docs.google.com/forms/d/12d8p9ckl-BJ1i0IuKnLXBQmhNZaOb-W20zEVngXO2uk";
           },
         });
       }
