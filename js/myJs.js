a804a5ba2ec56bc189f6eb76a054b522b7121ba5
thanhren <40245250+thanhren@users.noreply.github.com>
2021-10-23 14:23:49 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -3,15 +3,15 @@ const textConfig = {
   text2: "Bạn cũng đang tìm NY hả!",
   text3: "Vậy thì sao không thử tìm hiểu nhau ^^",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
+  text5: "Xin lỗi, không",
+  text6: "Oke tới luôn ^^",
+  text7: "Chỗ này chả có gì đâu!",
+  text8: "Nhấn vào đây nè!",
   text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text10: "Tuy không biết sẽ thế nào, nhưng mình cũng muốn là một mối quan hệ lâu dài",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Đừng bận tâm nhiều, nếu không hợp thì xem như mình chưa có duyên với nhau. Thoải mái nhé!",
+  text12: "Nhấn vào đây để mình biết bạn là ai nhé.",
 };
 
 $(document).ready(function () {
