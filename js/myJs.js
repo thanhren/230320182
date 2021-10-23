6dcc3cb37f718d3f218082e6d1b142b54d07c76f
thanhren <40245250+thanhren@users.noreply.github.com>
2021-10-23 14:25:16 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/cuteCat2.jpg",
+      imageUrl: "img/cuteCat.jpg",
       imageWidth: 300,
       imageHeight: 300,
       background: '#fff url("img/iput-bg.jpg")',
